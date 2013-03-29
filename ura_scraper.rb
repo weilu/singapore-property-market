70c61f39f7789afd75313e15f4f9f48673dca20d
Wei Lu <luwei.here@gmail.com>
2013-03-29 14:35:09 +0800

    Scrape data from 2007-06 to last month

--- ura_scraper.rb
+++ ura_scraper.rb
@@ -1,12 +1,44 @@
 require 'uri'
+require 'open-uri'
 require 'net/http'
 require 'nokogiri'
+require 'json'
 
-url = "http://www.ura.gov.sg/realEstateIIWeb/price/search.action"
-params = {
-  yearSelect: 2013,
-  monthSelect: 01
-}
-response = Net::HTTP.post_form(URI.parse(url), params)
-#puts response.body
-page = Nokogiri::HTML(response.body)
+require 'pry'
+
+data = []
+url = "http://www.ura.gov.sg/realEstateIIWeb/price/submitSearch.action"
+
+date_range = (Date.new(2007, 6)..Date.today.prev_month).select{|d| d.day == 1}
+date_range.each do |date|
+  year = date.year
+  month = date.strftime('%m')
+
+  response = Net::HTTP.post_form(URI.parse(url), yearSelect: year, monthSelect: month)
+  page = Nokogiri::HTML(response.body)
+
+  table = page.css('#SubmitSortForm table')
+  fields = table.css('table > thead > tr > td').map(&:text).map(&:strip)
+  rows = table.css('tr.rowalternate')
+
+  next if table.empty? || fields.empty? || rows.empty?
+
+  rows.each do |r|
+    values = r.css('td').map{|cell| cell.text.strip }
+    data << Hash[fields.zip values]
+  end
+  puts "done with #{year} #{month}"
+end
+
+File.open('data.json', 'w') do |f|
+  f.puts data.to_json
+end
+
+# helpers
+
+def save_and_open_page response
+  File.open('test.html', 'w') do |f|
+    f.puts response.body
+  end
+  `open test.html`
+end
